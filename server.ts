34d270cb9b99e0c159bf52ea07ca632676f524c6
salah mohamed <salah.mohamed@miu.edu>
2024-07-15 18:23:34 -0500

    [delete] cors

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 
 const app = express();
 const port = 4000;
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api', router);
